70dc91529ae29ac46109ad94a1c7dcbf6fab2bd5
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-10-24 17:46:37 +0530

    Add stack-trace log to the ApiError class

--- src/utils/custom-api-error.ts
+++ src/utils/custom-api-error.ts
@@ -2,7 +2,7 @@ import { IApiError } from "../types/types";
 
 // ApiError class extends the Error class from NodeJS and implements IApiError interface (ensures type-safety)
 export class ApiError extends Error implements IApiError {
-  errors?: any[] | undefined;
+  errors?: any[] | any | undefined;
   statusCode: number;
   stack?: string | undefined;
   type: string; // response type
@@ -13,7 +13,7 @@ export class ApiError extends Error implements IApiError {
     statusCode: number,
     type: string,
     message: string,
-    errors?: Error[]
+    errors?: Error[] | any | undefined
   ) {
     super(message);
     this.statusCode = statusCode;
@@ -21,5 +21,11 @@ export class ApiError extends Error implements IApiError {
     this.type = type;
     this.data = null;
     this.success = false;
+
+    // If there is a single error, then log its stack trace
+    if (errors instanceof Error) {
+      this.stack = errors.stack;
+      console.log(this.stack);
+    }
   }
 }
