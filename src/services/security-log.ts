302708d02bcc181e64a7f6c0a6c39f0b5cd6d32e
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-11-07 23:09:04 +0530

    Folder structure: Move OTP and SecurityLog services to a separate service folder

---
+++ src/services/security-log.ts
@@ -0,0 +1,8 @@
+class SecurityLog {
+  // Method to create a log-event of a user and project
+  // Method to clear all expired logs (older than M days)
+  // Method to fetch (first N) logs using userId
+  // Method to get all recent failed logins of a userId
+}
+
+export const securityLog = new SecurityLog();
